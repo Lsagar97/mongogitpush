295a1ad3a150872f0ce6b3f7c5125ed6abe21468
Lakshmi sagar P <likithjack57@gmail.com>
2022-11-11 17:50:59 +0530

    this is 1st git push through pychram

--- testgitpush.py
+++ testgitpush.py
@@ -1,1 +1,15 @@
-this is how changes are done through pycharm in github
+##this is how changes are done through pycharm in github
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Lsagar97:Likith97@cluster0.jvo7pib.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+d = {
+    "name":"likith",
+    "email":"likith@gamil.com",
+    "surname":"sagar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
