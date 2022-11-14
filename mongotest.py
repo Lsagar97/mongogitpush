dc329d4c6f9e659822039afe9c784d71b39e244b
Lakshmi sagar P <likithjack57@gmail.com>
2022-11-14 11:46:24 +0530

    this is 1st git push through pychram

--- mongotest.py
+++ mongotest.py
@@ -1,4 +1,4 @@
-import pymongo
+import pymongo ##(pip install mongp if not there after installing error if you get then pip install "pymongo[srv]" then all set)
 client = pymongo.MongoClient("mongodb+srv://Lsagar97:Likith97@cluster0.jvo7pib.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
