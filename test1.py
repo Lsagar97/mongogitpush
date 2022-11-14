608485a1bbf59802182a4ec3f61f6be8e8dd92aa
Lakshmi sagar P <likithjack57@gmail.com>
2022-11-14 14:22:05 +0530

    this is 1st git push through pychram

---
+++ test1.py
@@ -0,0 +1,22 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Lsagar97:Likith97@cluster0.jvo7pib.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+db1 = client['mongotest']
+coll=db1['test']
+
+#record = coll.find()
+#for i in record:
+#    print(i)
+
+#data = coll.find({'companyName': "likith-s"})
+#for i in data:
+ #   print(i)
+
+#data = coll.find({'courseoffered':{'$gt' : "E"}})
+#for i in data:
+ #   print(i)
+
+
+
+
